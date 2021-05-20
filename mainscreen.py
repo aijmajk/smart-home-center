838add8d169b7671776226f0dcbff410dc7389d7
Maksymilian Majchrzak <aijasd40000@gmail.com>
2021-05-20 21:28:19 +0200

    program working faster && weather top-right corner alligned

--- mainscreen.py
+++ mainscreen.py
@@ -8,9 +8,12 @@ class Screen(pygame.Surface):
     def __init__(self, screen_w, screen_h, font, fontcolor):
         pygame.Surface.__init__(self, (screen_w, screen_h))
 
-        # variables for updating weather forecast
-        self.time_passed = 0
-        self.updated = False
+        # variables for updating screen
+        self.hours_passed = 0
+        self.minutes_passed = 0
+        self.weather_updated = False
+        self.date_updated = False
+        self.week_updated = False
 
         # fonts
         screen_measurements = max(screen_w, screen_h)
@@ -39,15 +42,20 @@ class Screen(pygame.Surface):
         self.weekStartingHeight = self.timeStartingHeight + self.timeScreen.get_height()
         self.weatherStartingHeight = 0
 
+        # initial screens blit
         self.weatherScreen.blit(weatherForecast.update_weather(self.screen.get_width()/2, self.screen.get_height()/2, self.verySmallFont, self.fontcolor, self.verySmallHeight), (0, 0))
+        self.dateScreen.blit(self.update_date_screen(time.localtime()), (0, 0))
+        self.weekScreen.blit(self.update_week_screen(time.localtime()), (0, 0))
 
     def update_time_screen(self, current_time):
         """returns current time in H:M:S format"""
+
         hours = str(current_time[3])
         minutes = str(current_time[4])
         seconds = str(current_time[5])
 
-        self.time_passed = int(minutes)
+        self.hours_passed = int(hours)
+        self.minutes_passed = int(minutes)
 
         if len(hours) == 1:
             hours = "0"+hours
@@ -59,6 +67,8 @@ class Screen(pygame.Surface):
 
     def update_date_screen(self, current_time):
         """returns current date in string in 'D M Y' format"""
+
+        print("date screen updated")
         month_name = {
             1: "Stycznia",
             2: "Lutego",
@@ -81,6 +91,8 @@ class Screen(pygame.Surface):
 
     def update_week_screen(self, current_time):
         """returns current week in year"""
+
+        print("week screen updated")
         week = current_time[7]/7
         if week % 1 >= 0.5:
             week = str(int(week)+1)
@@ -88,24 +100,12 @@ class Screen(pygame.Surface):
             week = str(int(week))
         return self.verySmallFont.render(week+" tydzień roku", True, self.fontcolor)
 
-    def update_weather_screen(self):
-        """updates weather"""
-        if self.time_passed == 0 or self.time_passed == 30:
-            if not self.updated:
-                self.weatherScreen.blit(weatherForecast.update_weather(self.screen.get_width()/2, self.screen.get_height()/2, self.verySmallFont, self.fontcolor, self.verySmallHeight), (0, 0))
-                self.updated = True
-        else:
-            self.updated = False
-
     def draw(self):
         """Draws everything on itself, based on the info provided in class.
          After resolving this module, object is ready to blit on designated surface"""
         # CLEAR THE SCREENS
         self.fill((0, 0, 0))
         self.screen.fill((0, 0, 0))
-        self.timeScreen.fill((0, 0, 0))
-        self.dateScreen.fill((0, 0, 0))
-        self.weekScreen.fill((0, 0, 0))
 
         # self.fill((155, 0, 0))
         # self.screen.fill((0, 255, 0))
@@ -114,18 +114,38 @@ class Screen(pygame.Surface):
         # self.weekScreen.fill((0, 0, 200))
 
         # blit time screen
+        self.timeScreen.fill((0, 0, 0))
         self.timeScreen.blit(self.update_time_screen(time.localtime()), (0, 0))
-        # blit date screen
-        self.dateScreen.blit(self.update_date_screen(time.localtime()), (0, 0))
-        # blit week screen
-        self.weekScreen.blit(self.update_week_screen(time.localtime()), (0, 0))
-        # weather screen
-        self.update_weather_screen()
 
+        # blit date screen
+        if self.hours_passed == 0:
+            if not self.date_updated:
+                self.dateScreen.fill((0, 0, 0))
+                self.dateScreen.blit(self.update_date_screen(time.localtime()), (0, 0))
+                self.date_updated = True
+        else:
+            self.date_updated = False
+
+        # blit week screen
+        if self.hours_passed == 0:
+            if not self.week_updated:
+                self.weekScreen.fill((0, 0, 0))
+                self.weekScreen.blit(self.update_week_screen(time.localtime()), (0, 0))
+                self.week_updated = True
+        else:
+            self.date_updated = False
+
+        # weather screen
+        if self.minutes_passed == 0 or self.minutes_passed == 15 or self.minutes_passed == 30 or self.minutes_passed == 45:
+            if not self.weather_updated:
+                self.weatherScreen.blit(weatherForecast.update_weather(self.screen.get_width()/2, self.screen.get_height()/2, self.verySmallFont, self.fontcolor, self.verySmallHeight), (0, 0))
+                self.weather_updated = True
+        else:
+            self.weather_updated = False
 
         # BLIT EVERYTHING ONTO MAIN SCREEN
         self.screen.blit(self.dateScreen, (0, self.dateStartingHeight))
         self.screen.blit(self.timeScreen, (0, self.timeStartingHeight))
         self.screen.blit(self.weekScreen, (0, self.weekStartingHeight))
-        self.screen.blit(self.weatherScreen, (self.screen.get_width()/2, self.weatherStartingHeight))
+        self.screen.blit(self.weatherScreen, (self.screen.get_width()/2 + 120, self.weatherStartingHeight - 60))
         self.blit(self.screen, (50, 50))
